b0aa7f40036e4d0170604e0b188225e321afe56f
GeorgeU029 <georgeulloa.fl@gmail.com>
2025-01-29 18:25:45 -0500

    prepared the data for analyze

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ for cateogry in classes:
         img_array = cv2.imread(os.path.join(path,img))
         plt.imshow(cv2.cvtColor(img_array,cv2.COLOR_BGR2RGB))
         plt.show()
+        break
+    break
+img_size = 244
+new_array = cv2.resize(img_array,(img_size,img_size))
+plt.imshow(cv2.cvtColor(new_array,cv2.COLOR_BGR2RGB))
+plt.show()
+
+traning_data = []
+def create_training_data():
+    for category in classes:
+        path = os.path.join(data_directory,cateogry)
+        classes_num = classes.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path,img))
+                new_array = cv2.resize(img_array,(img_size,img_size))
+                traning_data.append([new_array,classes_num])
+            except Exception as e:
+                pass     
